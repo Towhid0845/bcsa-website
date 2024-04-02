96fe246ff78100bed7a0530798e759b0a270ba0a
towhid0845 <towhid0845@gmail.com>
2024-04-02 12:25:44 +0600

    design completed except member register form & event register form

--- js/script.js
+++ js/script.js
@@ -92,6 +92,35 @@ $(function () {
     $('select').niceSelect();
   })
 
+  // gallery popup js
+  $('.parent-container').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    },
+    removalDelay: 300,
+    mainClass: 'mfp-fade',
+  });
+
+  // video gallery popup js
+  $('.vidplay').magnificPopup({
+    type: 'iframe',
+    iframe: {
+      markup: '<div class="mfp-iframe-scaler">' +
+        '<div class="mfp-close"></div>' +
+        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+        '</div>',
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: 'v=',
+          src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+        },
+      },
+      srcAction: 'iframe_src',
+    }
+  });
 
   // NiceSelect.bind(document.getElementById("#a-select"));
   // Animation on scroll
